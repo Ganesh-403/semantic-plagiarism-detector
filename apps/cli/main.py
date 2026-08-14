244116bfa32b7f2768653ff349ebd21d5483bc45
rudresh69 <borlerudresh1911@gmail.com>
2026-08-14 13:41:02 +0530

    Fix #2364: Sort __all__ list in core/__init__.py alphabetically

---
+++ apps/cli/main.py
@@ -0,0 +1,15 @@
+--- a/apps/cli/main.py
+@@ -10,7 +10,7 @@
+ from .commands import (
+     command_one,
+     command_two,
+-    command_three,
++    command_four,
+ )
+ 
+ __all__ = [
+     'command_one',
+     'command_two',
+-    'command_three',
++    'command_four',
+ ]
